8a4523e19b8ec20c8b2a3350fc6b1778c5e3a551
enzohideo <enzohideo@users.noreply.github.com>
2024-09-14 15:58:22 -0300

    src: Add basic http server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
+import http from "node:http";
 import OpenAI from "openai";
 
+const MODEL = process.env["MODEL"];
+const HOST = process.env["HOST"];
+const PORT = process.env["PORT"];
+
+const messages = [];
+
 const client = new OpenAI({
-  apiKey: process.env["MARITALK_API_KEY"],
-  baseURL: "https://chat.maritaca.ai/api",
+  apiKey: process.env["API_KEY"],
+  baseURL: process.env["API_URL"],
 });
 
-async function main() {
-  const chatCompletion = await client.chat.completions.create({
-    messages: [{ role: "user", content: "Gere um código javascript usando html5 canvas para desenhar um sistema solar" }],
-    model: "sabia-3",
-  });
-  console.log(JSON.stringify(chatCompletion));
-}
+http
+  .createServer((req, res) => {
+    req.on("data", async (buffer) => {
+      const request = JSON.parse(buffer.toString("utf-8"));
 
-main();
+      if (!request || !request.content) {
+        res.statusCode = 400;
+        res.end();
+        return;
+      }
+
+      messages.push({
+        role: "user",
+        content: request.content,
+      });
+
+      const response = await client.chat.completions.create({
+        messages: messages,
+        model: MODEL,
+      });
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(response));
+    });
+  })
+  .listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}/`);
+  });
