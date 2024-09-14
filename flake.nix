e38f9e40500b25428731d5c41c4bc393c74937df
enzohideo <enzohideo@users.noreply.github.com>
2024-09-14 20:16:58 -0300

    nix: Add nix flake with basic dev shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Codelab Chatbot";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+      pkgsFor = eachSystem (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShell = eachSystem (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            nodePackages.npm
+          ];
+        }
+      );
+    };
+}
