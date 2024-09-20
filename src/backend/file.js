308ac074029c1fdc3e4af1d8bca5847e8a944447
enzohideo <enzohideo@users.noreply.github.com>
2024-09-20 16:45:52 -0300

    backend/file: Lint toBool

--- src/backend/file.js
+++ src/backend/file.js
@@ -22,8 +22,7 @@ export default async (base, url) => {
 
   const pathTraversal = !filePath.startsWith(base);
   const found =
-    !pathTraversal &&
-    (await fs.promises.access(filePath).then(...toBool));
+    !pathTraversal && (await fs.promises.access(filePath).then(...toBool));
 
   const streamPath = found ? filePath : base + "/pages/404/index.html";
 
